dbc47b0d9facf3e97047b2148811454608e3a065
davidauza.engineer <davidauza01@gmail.com>
2020-05-11 23:31:46 -0500

    Update static_pages_controller_spec.rb

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  render_views
+
   context 'when guest user' do
-    it 'renders :home template' do
+    before do
       get :home
+    end
+
+    it 'renders :home template' do
       expect(response).to render_template(:home)
+    end
+
+    it 'displays the right title' do
+      expect(response.body).to have_title('Inicio | Set Your Future', exact: true)
     end
   end
 end
